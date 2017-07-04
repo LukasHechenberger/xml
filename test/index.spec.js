49d115e14a7e745ceda08e7254c9a4fcb9c1f6e9
Lukas Hechenberger <admin@ls-age.com>
2017-07-04 09:31:05 +0200

    docs: Integrate module exports test

--- test/index.spec.js
+++ test/index.spec.js
@@ -3,18 +3,22 @@ import Builder from '../src/lib/Builder';
 import Parser from '../src/lib/Parser';
 
 describe('Module', function() {
+  /** @test {Builder} */
   it('should export Builder class', function() {
     expect(moduleExports.Builder, 'to be', Builder);
   });
 
+  /** @test {Parser} */
   it('should export Parser class', function() {
     expect(moduleExports.Parser, 'to be', Parser);
   });
 
+  /** @test {parse} */
   it('should export parse function', function() {
     expect(moduleExports.parse, 'to be a function');
   });
 
+  /** @test {build} */
   it('should export build function', function() {
     expect(moduleExports.build, 'to be a function');
   });
